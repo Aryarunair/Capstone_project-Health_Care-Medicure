a1207f77f6edd4bb752ad7a0331fa67841fbc215
Arya Arun Nair <134001379+Aryarunair@users.noreply.github.com>
2024-07-14 00:11:15 +0300

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -2,30 +2,84 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_vpc" "main" {
-  id = "vpc-0a19c641974f61822"
+# Create VPC
+resource "aws_vpc" "k8s_vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "K8s VPC"
+  }
 }
 
-resource "aws_subnet" "main" {
-  vpc_id            = aws_vpc.main.id
+# Create Internet Gateway 
+resource "aws_internet_gateway" "k8s_gw" {
+  vpc_id = aws_vpc.k8s_vpc.id
+
+  tags = {
+    Name = "K8s Gateway"
+  }
+}
+
+# Create Route Table
+resource "aws_route_table" "k8s_route_table" {
+  vpc_id = aws_vpc.k8s_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.k8s_gw.id
+  }
+
+  tags = {
+    Name = "K8s Route Table"
+  }
+}
+
+# Create Subnet
+resource "aws_subnet" "k8s_subnet" {
+  vpc_id            = aws_vpc.k8s_vpc.id
   cidr_block        = "10.0.1.0/24"
   availability_zone = "ap-south-1a"
+
+  tags = {
+    Name = "K8s Subnet"
+  }
 }
 
-resource "aws_security_group" "k8s_sg" {
-  vpc_id = aws_vpc.main.id
+# Create Security Group for K8s Master
+resource "aws_security_group" "k8s_master_sg" {
+  name   = "K8s Master Security Group"
+  vpc_id = aws_vpc.k8s_vpc.id
 
   ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    cidr_blocks     = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port   = 6443
-    to_port     = 6443
-    protocol    = "tcp"
+    from_port       = 6443 
+    to_port         = 6443
+    protocol        = "tcp"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0 
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+}
+
+# Create Security Group for K8s Worker
+resource "aws_security_group" "k8s_worker_sg" {
+  name   = "K8s Worker Security Group" 
+  vpc_id = aws_vpc.k8s_vpc.id
+ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -35,72 +89,62 @@ resource "aws_security_group" "k8s_sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-}
+ ingress {
+ description = "HTTPS traffic"
+ from_port = 443
+ to_port = 443
+ protocol = "tcp"
+ cidr_blocks = ["0.0.0.0/0"]
+ }
+ ingress {
+ description = "HTTP traffic"
+ from_port = 0
+ to_port = 65000
+ protocol = "tcp"
+ cidr_blocks = ["0.0.0.0/0"]
+ }
+ ingress {
+ description = "Allow port 80 inbound"
+ from_port   = 80
+ to_port     = 80
+ protocol    = "tcp"
+ cidr_blocks = ["0.0.0.0/0"]
+  }
+ egress {
+ from_port = 0
+ to_port = 0
+ protocol = "-1"
+ cidr_blocks = ["0.0.0.0/0"]
+ ipv6_cidr_blocks = ["::/0"]
+ }
+ 
 
+# Create EC2 Instance for K8s Master
 resource "aws_instance" "k8s_master" {
-  ami           = "ami-0b2f6494ff0b07a0e" # Amazon Linux 2 AMI (HVM), SSD Volume Type
+  ami           = "ami-0cca134ec43cf708f" 
   instance_type = "t2.micro"
-  key_name      = "healthcare.pem"
-  subnet_id     = aws_subnet.main.id
-  security_groups = [aws_security_group.k8s_sg.name]
+  key_name      = "healthcare"
 
+  subnet_id                   = aws_subnet.k8s_subnet.id
+  vpc_security_group_ids      = [aws_security_group.k8s_master_sg.id]
   associate_public_ip_address = true
 
   tags = {
-    Name = "K8sMaster"
-  }
-
-  user_data = <<-EOF
-              #!/bin/bash
-              yum update -y
-              yum install -y docker
-              service docker start
-              usermod -aG docker ec2-user
-              curl -sfL https://get.k3s.io | sh -
-              EOF
-}
-
-resource "aws_eip" "k8s_master_eip" {
-  instance = aws_instance.k8s_master.id
-}
-
-resource "null_resource" "retrieve_k3s_token" {
-  provisioner "remote-exec" {
-    inline = [
-      "sleep 60", # Wait for the master node to initialize
-      "scp -o StrictHostKeyChecking=no -i healthcare.pem ec2-user@${aws_instance.k8s_master.public_ip}:/var/lib/rancher/k3s/server/node-token ./node-token"
-    ]
-
-    connection {
-      type        = "ssh"
-      user        = "ec2-user"
-      private_key = file("healthcare.pem")
-      host        = aws_instance.k8s_master.public_ip
-    }
+    Name = "K8s Master"
   }
 }
 
+# Create EC2 Instance for K8s Worker  
 resource "aws_instance" "k8s_worker" {
-  ami           = "ami-0c2af51e265bd5e0e"
-  instance_type = "t2.micro"
-  key_name      = "healthcare.pem"
-  subnet_id     = aws_subnet.main.id
-  security_groups = [aws_security_group.k8s_sg.name]
+  ami           = "ami-0cca134ec43cf708f"
+  instance_type = "t2.micro" 
+  key_name      = "healthcare"
 
+  subnet_id                   = aws_subnet.k8s_subnet.id
+  vpc_security_group_ids      = [aws_security_group.k8s_worker_sg.id]
   associate_public_ip_address = true
 
   tags = {
-    Name = "K8sWorker"
+    Name = "K8s Worker"
   }
-
-  depends_on = [null_resource.retrieve_k3s_token]
-
-  user_data = <<-EOF
-              #!/bin/bash
-              yum update -y
-              yum install -y docker
-              service docker start
-              usermod -aG docker ec2-user
-              curl -sfL https://get.k3s.io | K3S_URL=https://${aws_instance.k8s_master.private_ip}:6443 K3S_TOKEN=$(cat ./node-token) sh -
-              EOF
 }
