1421407c1d8dec0507d9006a900d6bf55dd9b0f2
Arya Arun Nair <134001379+Aryarunair@users.noreply.github.com>
2024-07-14 23:08:24 +0300

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,283 +1,121 @@
-#Initialize Terraform
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
-}
-
-# Configure the AWS provider
 provider "aws" {
-  region = "ap-south-1"
+  region     = "ap-south-1"
+  access_key = "AKIA2UC3CBNXH7TF4SX3"
+  secret_key = "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"
 }
 # Creating a VPC
-resource "aws_vpc" "proj-vpc" {
- cidr_block = "10.0.0.0/16"
+resource "aws_vpc" "demo_vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "JBuild-Server"
+  }
 }
 
 # Create an Internet Gateway
-resource "aws_internet_gateway" "proj-ig" {
- vpc_id = aws_vpc.proj-vpc.id
- tags = {
- Name = "gateway1"
- }
+resource "aws_internet_gateway" "demo_ig" {
+  vpc_id = aws_vpc.demo_vpc.id
+  tags = {
+    Name = "JBuild-Server"
+  }
 }
 
 # Setting up the route table
-resource "aws_route_table" "proj-rt" {
- vpc_id = aws_vpc.proj-vpc.id
- route {
- # pointing to the internet
- cidr_block = "0.0.0.0/0"
- gateway_id = aws_internet_gateway.proj-ig.id
- }
- route {
- ipv6_cidr_block = "::/0"
- gateway_id = aws_internet_gateway.proj-ig.id
- }
- tags = {
- Name = "rt1"
- }
+resource "aws_route_table" "demo_rt" {
+  vpc_id = aws_vpc.demo_vpc.id
+
+  route {
+    # pointing to the internet
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.demo_ig.id
+  }
+  route {
+    ipv6_cidr_block = "::/0"
+    gateway_id = aws_internet_gateway.demo_ig.id
+  }
+  tags = {
+    Name = "JBuild-Server"
+  }
 }
 
 # Setting up the subnet
-resource "aws_subnet" "proj-subnet" {
- vpc_id = aws_vpc.proj-vpc.id
- cidr_block = "10.0.1.0/24"
- availability_zone = "ap-south-1b"
- tags = {
- Name = "subnet1"
- }
+resource "aws_subnet" "demo_subnet" {
+  vpc_id             = aws_vpc.demo_vpc.id
+  cidr_block         = "10.0.1.0/24"
+  availability_zone  = "ap-south-1b"
+  tags = {
+    Name = "JBuild-Server"
+  }
 }
 
 # Associating the subnet with the route table
-resource "aws_route_table_association" "proj-rt-sub-assoc" {
-subnet_id = aws_subnet.proj-subnet.id
-route_table_id = aws_route_table.proj-rt.id
+resource "aws_route_table_association" "proj_rt_sub_assoc" {
+  subnet_id      = aws_subnet.demo_subnet.id
+  route_table_id = aws_route_table.demo_rt.id
 }
 
 # Creating a Security Group
-resource "aws_security_group" "proj-sg" {
- name = "proj-sg"
- description = "Enable web traffic for the project"
- vpc_id = aws_vpc.proj-vpc.id
- ingress {
+resource "aws_security_group" "demo_sg" {
+  name        = "JBuild-Server"
+  description = "Enable all traffic for the project"
+  vpc_id      = aws_vpc.demo_vpc.id
+
+  ingress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port         = 0
+    to_port           = 0
+    protocol          = "-1"
+    cidr_blocks       = ["0.0.0.0/0"]
+    ipv6_cidr_blocks  = ["::/0"]
   }
- ingress {
- description = "HTTPS traffic"
- from_port = 443
- to_port = 443
- protocol = "tcp"
- cidr_blocks = ["0.0.0.0/0"]
- }
- ingress {
- description = "HTTP traffic"
- from_port = 0
- to_port = 65000
- protocol = "tcp"
- cidr_blocks = ["0.0.0.0/0"]
- }
- ingress {
- description = "Allow port 80 inbound"
- from_port   = 80
- to_port     = 80
- protocol    = "tcp"
- cidr_blocks = ["0.0.0.0/0"]
+
+  tags = {
+    Name = "JBuild-Server"
   }
- egress {
- from_port = 0
- to_port = 0
- protocol = "-1"
- cidr_blocks = ["0.0.0.0/0"]
- ipv6_cidr_blocks = ["::/0"]
- }
- tags = {
- Name = "proj-sg1"
- }
 }
 
 # Creating a new network interface
-resource "aws_network_interface" "proj-ni" {
- subnet_id = aws_subnet.proj-subnet.id
- private_ips = ["10.0.1.10"]
- security_groups = [aws_security_group.proj-sg.id]
+resource "aws_network_interface" "demo_ni" {
+  subnet_id       = aws_subnet.demo_subnet.id
+  private_ips     = ["10.0.1.10"]
+  security_groups = [aws_security_group.demo_sg.id]
+}
+
+# Creating an Ubuntu EC2 instance
+resource "aws_instance" "test_server" {
+  ami           = "ami-0c2af51e265bd5e0e"
+  instance_type = "t2.micro"
+  key_name      = "healthcare.pem"
+
+  network_interface {
+    device_index         = 0
+    network_interface_id = aws_network_interface.demo_ni.id
+  }
+  
+  user_data = <<-EOF
+  #!/bin/bash
+  sudo apt-get update -y
+  EOF
+
+  tags = {
+    Name = "JBuild-Server"
+  }
 }
 
 # Attaching an elastic IP to the network interface
-resource "aws_eip" "proj-eip" {
- vpc = true
- network_interface = aws_network_interface.proj-ni.id
- associate_with_private_ip = "10.0.1.10"
-}
+resource "aws_eip" "demo_eip" {
+  network_interface         = aws_network_interface.demo_ni.id
+  associate_with_private_ip = "10.0.1.10"
 
+  depends_on = [aws_instance.test_server]
 
-# Creating an ubuntu EC2 instance
-resource "aws_instance" "k8s_Master" {
- ami = "ami-0c2af51e265bd5e0e"
- instance_type = "t2.micro"
- availability_zone = "ap-south-1b"
- key_name = "healthcare.pem"
- network_interface {
- device_index = 0
- network_interface_id = aws_network_interface.proj-ni.id
- }
- user_data  = <<-EOF
- #!/bin/bash
-     sudo apt-get update -y
-    
- EOF
- tags = {
- Name = "k8s_Master"
- }
-}
-
-
-
-# Configure the AWS provider
-provider "aws" {
-  region = "ap-south-1"
-}
-# Creating a VPC
-resource "aws_vpc" "proj-vpc1" {
- cidr_block = "10.0.0.0/16"
-}
-
-# Create an Internet Gateway
-resource "aws_internet_gateway" "proj-ig1" {
- vpc_id = aws_vpc.proj-vpc1.id
- tags = {
- Name = "gateway2"
- }
-}
-
-# Setting up the route table
-resource "aws_route_table" "proj-rt1" {
- vpc_id = aws_vpc.proj-vpc1.id
- route {
- # pointing to the internet
- cidr_block = "0.0.0.0/0"
- gateway_id = aws_internet_gateway.proj-ig1.id
- }
- route {
- ipv6_cidr_block = "::/0"
- gateway_id = aws_internet_gateway.proj-ig1.id
- }
- tags = {
- Name = "rt2"
- }
-}
-
-# Setting up the subnet
-resource "aws_subnet" "proj-subnet1" {
- vpc_id = aws_vpc.proj-vpc1.id
- cidr_block = "10.0.1.0/24"
- availability_zone = "ap-south-1b"
- tags = {
- Name = "subnet2"
- }
-}
-
-# Associating the subnet with the route table
-resource "aws_route_table_association" "proj-rt-sub-assoc1" {
-subnet_id = aws_subnet.proj-subnet1.id
-route_table_id = aws_route_table.proj-rt1.id
-}
-
-# Creating a Security Group
-resource "aws_security_group" "proj-sg1" {
- name = "proj-sg1"
- description = "Enable web traffic for the project"
- vpc_id = aws_vpc.proj-vpc.id
- ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+  tags = {
+    Name = "JBuild-Server"
   }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
- ingress {
- description = "HTTPS traffic"
- from_port = 443
- to_port = 443
- protocol = "tcp"
- cidr_blocks = ["0.0.0.0/0"]
- }
- ingress {
- description = "HTTP traffic"
- from_port = 0
- to_port = 65000
- protocol = "tcp"
- cidr_blocks = ["0.0.0.0/0"]
- }
- ingress {
- description = "Allow port 80 inbound"
- from_port   = 80
- to_port     = 80
- protocol    = "tcp"
- cidr_blocks = ["0.0.0.0/0"]
-  }
- egress {
- from_port = 0
- to_port = 0
- protocol = "-1"
- cidr_blocks = ["0.0.0.0/0"]
- ipv6_cidr_blocks = ["::/0"]
- }
- tags = {
- Name = "proj-sg1"
- }
-}
-
-# Creating a new network interface
-resource "aws_network_interface" "proj-ni1" {
- subnet_id = aws_subnet.proj-subnet1.id
- private_ips = ["10.0.1.10"]
- security_groups = [aws_security_group.proj-sg1.id]
-}
-
-# Attaching an elastic IP to the network interface
-resource "aws_eip" "proj-eip1" {
- vpc = true
- network_interface = aws_network_interface.proj-ni1.id
- associate_with_private_ip = "10.0.1.102"
-}
-
-
-# Creating an ubuntu EC2 instance
-resource "aws_instance" "k8s_worker" {
- ami = "ami-0c2af51e265bd5e0e"
- instance_type = "t2.micro"
- availability_zone = "ap-south-1b"
- key_name = "healthcare.pem"
- network_interface {
- device_index = 0
- network_interface_id = aws_network_interface.proj-ni1.id
- }
- user_data  = <<-EOF
- #!/bin/bash
-     sudo apt-get update -y
-   
- EOF
- tags = {
- Name = "k8s_worker"
- }
 }
